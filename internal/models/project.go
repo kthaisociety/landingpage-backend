1b061aa20508d4c8eca8c6357aa14093779ed766
Daniel Ibrahimi <89932502+Daniel3178@users.noreply.github.com>
2026-05-03 10:10:43 +0200

    Feat admin project integration (#106)
    * feat: update the database schemas to meet the requirements
    
    * feat: update main to follow the new schema
    
    * feat: update project_handler to create, update and delete a project. AddMember and RemoveMember are no longer needed.
    
    * fix: resolve PR feedback
    
    * fix: resolve orphane teams in delete handler
    
    * fix: resolve PR request regarding db constraints

--- internal/models/project.go
+++ internal/models/project.go
@@ -2,23 +2,43 @@ package models
 
 import (
 	"github.com/google/uuid"
-	"github.com/lib/pq"
+	// "github.com/lib/pq"
 	"gorm.io/gorm"
 )
 
 type ProjectStatus string
 
+// const (
+// 	ProjectStatusPlanning  ProjectStatus = "planning"
+// 	ProjectStatusActive    ProjectStatus = "active"
+// 	ProjectStatusCompleted ProjectStatus = "completed"
+// )
+
 const (
-	ProjectStatusPlanning  ProjectStatus = "planning"
-	ProjectStatusActive    ProjectStatus = "active"
-	ProjectStatusCompleted ProjectStatus = "completed"
+	ProjectStatusIdea        ProjectStatus = "Idea"
+	ProjectStatusPrototype   ProjectStatus = "Prototype"
+	ProjectStatusDevelopment ProjectStatus = "In development"
+	ProjectStatusBeta        ProjectStatus = "Public beta"
+	ProjectStatusLive        ProjectStatus = "Live"
 )
 
 type Project struct {
 	gorm.Model
-	ProjectId   uuid.UUID      `gorm:"uniqueIndex" json:"project_id"`
-	ProjectName string         `gorm:"not null" json:"name"`
-	Description string         `gorm:"type:text" json:"description"`
-	Skills      pq.StringArray `gorm:"type:text[]" json:"skills"`
-	Status      ProjectStatus  `gorm:"type:varchar(20);default:'planning'" json:"status"`
+	ProjectId   uuid.UUID `gorm:"uniqueIndex;default:gen_random_uuid()" json:"project_id"`
+	ProjectName string    `gorm:"not null" json:"title"`
+	// Description string         `gorm:"type:text" json:"description"`
+	// Skills      pq.StringArray `gorm:"type:text[]" json:"skills"`
+	// Status      ProjectStatus  `gorm:"type:varchar(20);default:'planning'" json:"status"`
+	OneLineDescription string        `gorm:"not null" json:"one_line_description"`
+	Categories         string        `gorm:"not null" json:"categories"`
+	TechStack          string        `gorm:"not null" json:"tech_stack"`
+	ProblemImpact      string        `gorm:"type:text;not null" json:"problem_impact"`
+	KeyFeatures        string        `gorm:"type:text;not null" json:"key_features"` // JSON stringified array of strings with <SEP> as delimiter
+	Status             ProjectStatus `gorm:"type:varchar(50);not null" json:"status"`
+	Screenshots        string        `gorm:"type:text" json:"screenshots"` // JSON stringified array of object
+	RepoUrl            string        `json:"repo_url"`
+	Affiliations       string        `json:"affiliations"`
+	Timeline           string        `gorm:"type:text" json:"timeline"` // JSON stringified object
+	MaintenancePlan    string        `gorm:"type:text" json:"maintenance_plan"`
+	Contact            string        `json:"contact"`
 }
